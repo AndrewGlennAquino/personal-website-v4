e13171bad77191d74c0c10d21f06a2372fba67db
Andrew Aquino <andrew.aquino20@icloud.com>
2026-07-02 18:34:28 -0400

    include links for accessibility

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -11,6 +11,20 @@ export function Home() {
         <h2 className="text-transparent absolute inset-0 -z-10">
           Full Stack Engineer
         </h2>
+        <a
+          className="text-transparent absolute inset-0 -z-10"
+          href="https://github.com/AndrewGlennAquino"
+          target="_blank"
+        >
+          Link to GitHub
+        </a>
+        <a
+          className="text-transparent absolute inset-0 -z-10"
+          href="https://www.linkedin.com/in/andrew-glenn-aquino"
+          target="_blank"
+        >
+          Link to LinkedIn
+        </a>
       </section>
     </main>
   );

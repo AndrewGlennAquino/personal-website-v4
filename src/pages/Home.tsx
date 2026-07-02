05c355817eaf9d38bed0194193a4451b7f119a9c
Andrew Aquino <andrew.aquino20@icloud.com>
2026-07-02 18:40:03 -0400

    feat: include links for accessibility

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -13,14 +13,14 @@ export function Home() {
         </h2>
         <a
           className="text-transparent absolute inset-0 -z-10"
-          href="https://github.com/AndrewGlennAquino"
+          href="https://github.com/AndrewGlennAquino/"
           target="_blank"
         >
           Link to GitHub
         </a>
         <a
           className="text-transparent absolute inset-0 -z-10"
-          href="https://www.linkedin.com/in/andrew-glenn-aquino"
+          href="https://www.linkedin.com/in/andrew-glenn-aquino/"
           target="_blank"
         >
           Link to LinkedIn
